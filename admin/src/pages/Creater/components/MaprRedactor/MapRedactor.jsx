c31ce3ba3ffd6e5bbaf7564481f8db5fe39207a8
Александр Утбанов <utbanovag@it2g.ru>
2020-09-26 02:07:10 +0500

    add tooltip

--- admin/src/pages/Creater/components/MaprRedactor/MapRedactor.jsx
+++ admin/src/pages/Creater/components/MaprRedactor/MapRedactor.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from "react";
+import { Tooltip } from "antd";
 
 import "./style.scss";
 
@@ -13,14 +14,16 @@ export const MapRedactor = (props) => {
         <div className="map-redactor">
             {area.map((row, i) => 
                 <div className="map-row"key={i}>
-                    {row.map((cell, j) => 
-                        <div 
-                            className="map-cell"
-                            key={`${i}_${j}`}
-                            onClick={() => handleSet(cell)}
-                            style={{ backgroundColor: cell.color }} 
-                        ></div>
-                    )}
+                    {row.map((cell, j) => (
+                        <Tooltip title={cell.name}>
+                            <div 
+                                className="map-cell"
+                                key={`${i}_${j}`}
+                                onClick={() => handleSet(cell)}
+                                style={{ backgroundColor: cell.color }} 
+                            ></div>
+                        </Tooltip>
+                    ))}
                 </div>
             )}
         </div>
