e9068e21f94d187c68620ef110d61cc88fac2206
Александр Утбанов <utbanovag@it2g.ru>
2020-09-26 13:16:09 +0500

    add capacitor config

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import React, { useState } from 'react';
+import { View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import { PANELS } from "./shared/constants";
@@ -9,7 +7,6 @@ import { LibraryList, LibraryMap, Auhtorization } from './panels/';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState(PANELS.authorization);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
 	return (
 		<View activePanel={activePanel}>
@@ -21,24 +18,3 @@ const App = () => {
 }
 
 export default App;
-
-// FOR EXAMPLE FATCH USER
-/**
- * useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
-
-	
- */
