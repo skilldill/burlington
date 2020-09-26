94f95471d36759d95b07464dd261edee097129aa
Александр Утбанов <utbanovag@it2g.ru>
2020-09-26 14:24:38 +0500

    add qr reader

--- mobile/src/panels/LibraryMap/LibraryMap.jsx
+++ mobile/src/panels/LibraryMap/LibraryMap.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from "react";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 import {Panel, PanelHeader, Group, PanelHeaderBack, Div, Button} from '@vkontakte/vkui';
 import Icon24Qr from '@vkontakte/icons/dist/24/qr';
 
@@ -6,11 +7,18 @@ import { PANELS } from "../../shared/constants";
 import { MapControl } from "./components";
 
 export const LibraryMap = (props) => {
+    const [code, setCode] = useState('');
+
     const { id, go } = props;
 
     const handleBack = useCallback(() => {
         !!go && go(PANELS.libraryList);
     }, [go])
+
+    const openBarCode = useCallback(async () => {
+        const data = await BarcodeScanner.scan();
+        setCode(`Barcode data: ${data.text}`);
+    }, [setCode])
 
     return (
         <Panel id={id}>
@@ -20,7 +28,11 @@ export const LibraryMap = (props) => {
             <Group>
                 <Div>
                     <MapControl />
-                    <Button size="xl" before={<Icon24Qr />}>Занять место</Button>
+                    <Button 
+                        size="xl" 
+                        before={<Icon24Qr />}
+                        onClick={openBarCode}
+                    >Занять место</Button>
                 </Div>
             </Group>
         </Panel>
